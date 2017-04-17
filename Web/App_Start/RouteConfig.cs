f858f07425d8f3d620c4f5fdd9bda12cbc19af85
王成飞 <764297968@qq.com>
2017-04-17 17:33:45 +0800

    11

---
+++ Web/App_Start/RouteConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Web
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Index",
+                "index.html",
+                 new { controller = "Home", action = "Index" }
+            );
+            routes.MapRoute(
+               "IDHtml", // id伪静态    
+               "{controller}/{action}/{id}.html",// 带有参数的 URL    
+               new { controller = "Home", action = "Index", id = UrlParameter.Optional }// 参数默认值    
+            );
+            routes.MapRoute(
+               "ActionHtml", // id伪静态    
+               "{controller}/{action}.html",// 带有参数的 URL    
+               new { controller = "Home", action = "Index", id = UrlParameter.Optional }// 参数默认值    
+            );
+             routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}",
+                defaults: new { controller = "Home", action = "Index" }
+            ); 
+        }
+    }
+}
---
+++ Web/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            if (Context.Request.FilePath == "/")
+                Context.RewritePath("index.html");
+        }
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+        protected void Application_Error()
+        {
+            Exception err = HttpContext.Current.Error;
+            string str = err.Message + err.StackTrace+ "\r\n------------------------------------\r\n";
+            System.IO.File.AppendAllText(Server.MapPath("/Logs")+"/"+DateTime.Now.ToString("yyyyMMdd")+".txt",str);
+        }
+    }
+}
